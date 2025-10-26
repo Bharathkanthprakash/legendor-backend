1ce78518d184ffe0358a2f4e8dd23c93feb8637e
Bharathkanthprakash <164721310+Bharathkanthprakash@users.noreply.github.com>
2025-10-27 04:37:50 +0530

    Update emailService.js

--- utils/emailService.js
+++ utils/emailService.js
@@ -27,4 +27,29 @@ export const sendEmail = async (to, subject, text, html) => {
   }
 };
 
+// Add the missing exports that auth.js is looking for
+export const sendVerificationEmail = async (to, token) => {
+  const verificationUrl = `${process.env.FRONTEND_URL}/verify-email?token=${token}`;
+  const subject = 'Verify Your Email - Legendor';
+  const html = `
+    <h2>Welcome to Legendor!</h2>
+    <p>Please verify your email by clicking the link below:</p>
+    <a href="${verificationUrl}">Verify Email</a>
+    <p>Or copy this link: ${verificationUrl}</p>
+  `;
+  
+  return await sendEmail(to, subject, '', html);
+};
+
+export const sendWelcomeEmail = async (to, username) => {
+  const subject = 'Welcome to Legendor!';
+  const html = `
+    <h2>Welcome to Legendor, ${username}!</h2>
+    <p>Thank you for joining our community. We're excited to have you on board!</p>
+    <p>Start exploring and connecting with other legends.</p>
+  `;
+  
+  return await sendEmail(to, subject, '', html);
+};
+
 export default transporter;

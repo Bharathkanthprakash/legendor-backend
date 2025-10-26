2baefe471b28aafe14464258d834821892754282
Bharathkanthprakash <164721310+Bharathkanthprakash@users.noreply.github.com>
2025-10-26 18:31:33 +0530

    Create Like.js

---
+++ models/Like.js
@@ -0,0 +1,5 @@
+const likeSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
+  createdAt: { type: Date, default: Date.now }
+});

6f3476fa58cef88e46df6c3395a2c2d7b190d2d1
Bharathkanthprakash <164721310+Bharathkanthprakash@users.noreply.github.com>
2025-10-26 18:34:35 +0530

    Add comment schema for MongoDB using Mongoose

---
+++ models/Comment.js
@@ -0,0 +1,6 @@
+const commentSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
+  text: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
